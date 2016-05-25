c2f28899e5beecf2884e2f699f072af1ba9d9ba7
Meri Winchester <mwinchester@mchenry.edu>
2016-05-25 12:30:47 -0500

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,20 @@
+
+function change01(){
+ document.getElementById("large").src="images/eli01.jpg";
+}
+
+function change02(){
+    document.getElementById("large").src="images/eliollie1.jpg";
+}
+
+function change03(){
+    document.getElementById("large").src="images/ender01.jpg";
+}
+
+function change04(){
+    document.getElementById("large").src="images/nessie01.jpg";
+}
+
+function change05(){
+    document.getElementById("large").src="images/nosey.jpg";
+}
